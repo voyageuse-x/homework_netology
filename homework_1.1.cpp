2df1af45b59729d8c3f78721475774715bbb9c98
voyageuse <ksenia.sytch@gmail.com>
2025-02-09 14:43:20 +0300

    Task 1. Months

--- homework_1.1.cpp
+++ homework_1.1.cpp
@@ -21,50 +21,50 @@ int main() {
 	setlocale(LC_ALL, "ru");
 	int month{};
 	do {
-		std::cout << "Ââåäèòå íîìåð ìåñÿöà: ";
+		std::cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ Ð½Ð¾Ð¼ÐµÑ€ Ð¼ÐµÑÑÑ†Ð°: ";
 		std::cin >> month;
 		switch (month) {
 		case 0:
-			std::cout << "Äî ñâèäàíèÿ" << std::endl;
+			std::cout << "Ð”Ð¾ ÑÐ²Ð¸Ð´Ð°Ð½Ð¸Ñ" << std::endl;
 			break;
 		case january:
-			std::cout << "ßíâàðü" << std::endl;
+			std::cout << "Ð¯Ð½Ð²Ð°Ñ€ÑŒ" << std::endl;
 			break;
 		case february:
-			std::cout << "Ôåâðàëü" << std::endl;
+			std::cout << "Ð¤ÐµÐ²Ñ€Ð°Ð»ÑŒ" << std::endl;
 			break;
 		case march:
-			std::cout << "Ìàðò" << std::endl;
+			std::cout << "ÐœÐ°Ñ€Ñ‚" << std::endl;
 			break;
 		case april:
-			std::cout << "Àïðåëü" << std::endl;
+			std::cout << "ÐÐ¿Ñ€ÐµÐ»ÑŒ" << std::endl;
 			break;
 		case may:
-			std::cout << "Ìàé" << std::endl;
+			std::cout << "ÐœÐ°Ð¹" << std::endl;
 			break;
 		case june:
-			std::cout << "Èþíü" << std::endl;
+			std::cout << "Ð˜ÑŽÐ½ÑŒ" << std::endl;
 			break;
 		case july:
-			std::cout << "Èþëü" << std::endl;
+			std::cout << "Ð˜ÑŽÐ»ÑŒ" << std::endl;
 			break;
 		case august:
-			std::cout << "Àâãóñò" << std::endl;
+			std::cout << "ÐÐ²Ð³ÑƒÑÑ‚" << std::endl;
 			break;
 		case september:
-			std::cout << "Ñåíòÿáðü" << std::endl;
+			std::cout << "Ð¡ÐµÐ½Ñ‚ÑÐ±Ñ€ÑŒ" << std::endl;
 			break;
 		case october:
-			std::cout << "Îêòÿáðü" << std::endl;
+			std::cout << "ÐžÐºÑ‚ÑÐ±Ñ€ÑŒ" << std::endl;
 			break;
 		case november:
-			std::cout << "Íîÿáðü" << std::endl;
+			std::cout << "ÐÐ¾ÑÐ±Ñ€ÑŒ" << std::endl;
 			break;
 		case december:
-			std::cout << "Äåêàáðü" << std::endl;
+			std::cout << "Ð”ÐµÐºÐ°Ð±Ñ€ÑŒ" << std::endl;
 			break;
 		default:
-			std::cout << "Íåïðàâèëüíûé íîìåð!" << std::endl;
+			std::cout << "ÐÐµÐ¿Ñ€Ð°Ð²Ð¸Ð»ÑŒÐ½Ñ‹Ð¹ Ð½Ð¾Ð¼ÐµÑ€!" << std::endl;
 			
 		}
 	}
