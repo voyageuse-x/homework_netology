c5f836f6bb29ce5ab1410ff886fbe7ed95678cdf
voyageuse <ksenia.sytch@gmail.com>
2025-02-09 15:17:33 +0300

    Task 3. Address

--- homework_1.3.cpp
+++ homework_1.3.cpp
@@ -8,20 +8,21 @@ struct address {
 	int index;
 };
 void print_struct(address adr) {
-		std::cout << "Ãîðîä: " << adr.city << std::endl;
-		std::cout << "Óëèöà: " << adr.street << std::endl;
-		std::cout << "Íîìåð äîìà: " << adr.house_number << std::endl;
-		std::cout << "Íîìåð êâàðòèðû: " << adr.flat_number << std::endl;
-		std::cout << "Èíäåêñ: " << adr.index << std::endl;
+		std::cout << "Город: " << adr.city << std::endl;
+		std::cout << "Улица: " << adr.street << std::endl;
+		std::cout << "Номер дома: " << adr.house_number << std::endl;
+		std::cout << "Номер квартиры: " << adr.flat_number << std::endl;
+		std::cout << "Индекс: " << adr.index << std::endl;
 		return;
 	}
 int main() {
 	setlocale(LC_ALL, "ru");
-	address adr1 = {"Ìîñêâà", "Àðáàò", 12, 8, 123456};
-	address adr2 = {"Èæåâñê", "Ïóøêèíà", 59, 143, 953769};
+	address adr1 = {"Москва", "Арбат", 12, 8, 123456};
+	address adr2 = {"Ижевск", "Пушкина", 59, 143, 953769};
 	print_struct(adr1);
 	std::cout << std::endl;
 	print_struct(adr2);
 	
+
 	return EXIT_SUCCESS;
 }
