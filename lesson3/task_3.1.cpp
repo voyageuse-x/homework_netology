e23c6b0ee30482bc2ab34bc89593e8e2a0242765
voyageuse <ksenia.sytch@gmail.com>
2025-02-14 12:01:53 +0300

    Update task_3.1.cpp

--- lesson3/task_3.1.cpp
+++ lesson3/task_3.1.cpp
@@ -2,31 +2,55 @@
 #include <fstream>
 
 class address {
-public:
+
 	std::string city;
 	std::string street;
-	int numb_house;
-	int numb_flat;
+	std::string numb_house;
+	std::string numb_flat;
+public:
+	address() {
+		city = "city";
+		street = "street";
+		numb_house = 1;
+		numb_flat = 1;
+	}
+	void set_address(std::string city, std::string street, std::string numb_house, std::string numb_flat) {
+		this->city = city;
+		this->street = street;
+		this->numb_house = numb_house;
+		this->numb_flat = numb_flat;
+
+	}
+	std::string get_address() {
+		return city + ", " + street + ", " + numb_house + ", " + numb_flat;
+	}
 };
 
 int main() {
 	int count_adr;
+
+	std::string city;
+	std::string street;
+	std::string numb_house;
+	std::string numb_flat;
+
 	std::ifstream f_in("in.txt");
 	f_in >> count_adr;
 
-	address *adr = new address[count_adr];
-	
+	address* adr = new address[count_adr];
+
 	for (int i = 0; i < count_adr; i++) {
-		f_in >> adr[i].city;
-		f_in >> adr[i].street;
-		f_in >> adr[i].numb_house;
-		f_in >> adr[i].numb_flat;
+		f_in >> city;
+		f_in >> street;
+		f_in >> numb_house;
+		f_in >> numb_flat;
+		adr[i].set_address(city, street, numb_house, numb_flat);
 	}
 	f_in.close();
 	std::ofstream f_out("out.txt");
 	f_out << count_adr << std::endl;
 	for (int i = count_adr - 1; i >= 0; i--) {
-		f_out << adr[i].city << ", " << adr[i].street << ", " << adr[i].numb_house << ", " << adr[i].numb_flat << std::endl;
+		f_out << adr[i].get_address() << std::endl; 
 
 	}
 	f_out.close();
