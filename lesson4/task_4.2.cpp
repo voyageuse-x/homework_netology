7a6fbd5fd5051829cb423db94025a947dac1fdd5
Xenia <xenia.sytch@icloud.com>
2025-02-22 15:15:25 +0300

    Added tasks 2 and 3'

---
+++ lesson4/task_4.2.cpp
@@ -0,0 +1,200 @@
+﻿#include <iostream>
+#include <string>
+
+
+class Triangle {
+protected:
+	std::string name = "Треугольник";
+	int a, b, c;
+	int A, B, C;
+public:
+	Triangle() {
+		a = 10;
+		b = 20;
+		c = 30;
+		A = 50;
+		B = 60;
+		C = 70;
+	}
+	std::string get_name() {
+		return name;
+	}
+	int get_a() {
+		return a;
+	}
+	int get_b() {
+		return b;
+	}
+	int get_c() {
+		return c;
+	}
+	int get_A() {
+		return A;
+	}
+	int get_B() {
+		return B;
+	}
+	int get_C() {
+		return C;
+	}
+};
+
+class Right_angled_triangle : public Triangle {
+public:
+	Right_angled_triangle() {
+		name = "Прямоугольный треугольник";
+		C = 90;
+	}
+};
+class Isosceles_triangle : public Triangle {
+public:
+	Isosceles_triangle() {
+		name = "Равнобедренный треугольник";
+		c = a;
+		A = C;
+	}
+};
+class Equilateral_triangle : public Triangle {
+public:
+	Equilateral_triangle() {
+		name = "Равносторонний треугольний";
+		a = b = c;
+		A = B = C = 60;
+	}
+};
+
+
+class Quadrilateral {
+protected:
+	std::string name = "Четырехугольник";
+	int a, b, c, d;
+	int A, B, C, D;
+public:
+	Quadrilateral() {
+		a = 10;
+		b = 20;
+		c = 30;
+		d = 40;
+		A = 50;
+		B = 60;
+		C = 70;
+		D = 80;
+	}
+	std::string get_name() {
+		return name;
+	}
+	int get_a() {
+		return a;
+	}
+	int get_b() {
+		return b;
+	}
+	int get_c() {
+		return c;
+	}
+	int get_d() {
+		return d;
+	}
+	int get_A() {
+		return A;
+	}
+	int get_B() {
+		return B;
+	}
+	int get_C() {
+		return C;
+	}
+	int get_D() {
+		return D;
+	}
+
+};
+class Rectangle : public Quadrilateral{
+public:
+	Rectangle() {
+		name = "Прямоугольник";
+		a = c;
+		b = d;
+		A = B = C = D;
+	}
+};
+class Quadrant : public Quadrilateral {
+public:
+	Quadrant() {
+		name = "Квадрат";
+		a = b = c = d;
+		A = B = C = D = 90;
+	}
+};
+class Parallelogram : public Quadrilateral {
+public:
+	Parallelogram() {
+		name = "Параллелограмм";
+		a = c;
+		b = d;
+		A = C;
+		B = D;
+	}
+};
+class Rhomb : public Quadrilateral {
+public:
+	Rhomb() {
+		name = "Ромб";
+		a = b = c = d;
+		A = C;
+		B = D;
+
+	}
+};
+void print_info_triangle(Triangle *ptr_class) {
+	std::cout << "\n" << ptr_class->get_name() << ": \nСтороны: a = " << ptr_class->get_a() << " b = " << ptr_class->get_b() << " c = " << ptr_class->get_c() << std::endl;
+	std::cout << "Углы: A = " << ptr_class->get_A() << " B = " << ptr_class->get_B() << " C = " << ptr_class->get_C() << std::endl;
+}
+
+void print_info_rectangle(Quadrilateral* ptr_class) {
+	std::cout << "\n" << ptr_class->get_name() << ": \nСтороны: a = " << ptr_class->get_a() << " b = " << ptr_class->get_b() << " c = " << ptr_class->get_c() << " d = " << ptr_class->get_d() << std::endl;
+	std::cout << "Углы: A = " << ptr_class->get_A() << " B = " << ptr_class->get_B() << " C = " << ptr_class->get_C() << " D = " << ptr_class->get_D() << std::endl;
+
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	Triangle tr;
+	Triangle* ptr_tr = &tr;
+	print_info_triangle(ptr_tr);
+
+	Right_angled_triangle rat;
+	Triangle* ptr_rat = &rat;
+	print_info_triangle(ptr_rat);
+
+	Isosceles_triangle it;
+	Triangle* ptr_it = &it;
+	print_info_triangle(ptr_it);
+
+	Equilateral_triangle et;
+	Triangle* ptr_et = &et;
+	print_info_triangle(ptr_et);
+
+	Quadrilateral ql;
+	Quadrilateral* ptr_ql = &ql;
+	print_info_rectangle(ptr_ql);
+
+
+	Rectangle rec;
+	Quadrilateral* ptr_rec = &rec;
+	print_info_rectangle(ptr_rec);
+
+	Quadrant quadr;
+	Quadrilateral* ptr_quadr = &quadr;
+	print_info_rectangle(ptr_quadr);
+
+	Parallelogram par;
+	Quadrilateral* ptr_par = &par;
+	print_info_rectangle(ptr_par);
+
+	Rhomb rhomb;
+	Quadrilateral* ptr_rhomb = &rhomb;
+	print_info_rectangle(ptr_rhomb);
+
+	return EXIT_SUCCESS;
+}
---
+++ lesson4/task_4.3.cpp
@@ -0,0 +1,261 @@
+﻿#include <iostream>
+#include <string>
+
+class Figure {
+protected:
+	int count_sides;
+	std::string name;
+public:
+	Figure() {
+		count_sides = 0;
+		name = "Фигура";
+	}
+
+	virtual bool check() {
+		if (count_sides == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	virtual void print_info() {
+		std::cout << "\n" << name << ": " << std::endl;
+		if (check()) {
+			std::cout << "Правильный" << std::endl;
+		}
+		else {
+			std::cout << "Неправильный" << std::endl;
+		}
+		std::cout << "Количество сторон: " << count_sides << std::endl;
+		return;
+	}
+};
+
+class Triangle : public Figure {
+protected:
+	int a, b, c;
+	int A, B, C;
+public:
+	Triangle() {
+		count_sides = 3;
+		name = "Треугольник";
+		a = 10;
+		b = 20;
+		c = 30;
+		A = 50;
+		B = 60;
+		C = 70;
+	}
+	void print_info() override{
+		Figure::print_info();
+		std::cout << "Стороны: a = " << a << " b = " << b << " c = " << c << std::endl;
+		std::cout << "Углы: A = " << A << " B = " << B << " C = " << C << std::endl;
+		return;
+	}
+
+	virtual bool check() override {
+		if (count_sides == 3 && (A + B + C) == 180) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Right_angled_triangle : public Triangle {
+public:
+	Right_angled_triangle() {
+		name = "Прямоугольный треугольник";
+		C = 90;
+	}
+	bool check() override {
+		if (C == 90) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Isosceles_triangle : public Triangle {
+public:
+	Isosceles_triangle() {
+		name = "Равнобедренный треугольник";
+		c = a;
+		A = C;
+	}
+	bool check() override {
+		if (a == c && A == C) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Equilateral_triangle : public Triangle {
+public:
+	Equilateral_triangle() {
+		name = "Равносторонний треугольний";
+		a = b = c;
+		A = B = C = 60;
+	}
+	bool check() override {
+		if (a == c && b == c && A == 60 && B == 60 && C == 60) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+class Quadrilateral : public Figure{
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+public:
+	Quadrilateral() {
+		name = "Четырехугольник";
+		count_sides = 4;
+		a = 10;
+		b = 20;
+		c = 30;
+		d = 40;
+		A = 50;
+		B = 60;
+		C = 70;
+		D = 80;
+	}
+	void print_info() override {
+		Figure::print_info();
+		std::cout << "Стороны: a = " << a << " b = " << b << " c = " << c << " d = " << d << std::endl;
+		std::cout << "Углы: A = " << A << " B = " << B << " C = " << C << " D = " << D << std::endl;
+		return;
+	}
+
+	virtual bool check() override {
+		if (count_sides == 4 && (A + B + C + D) == 360) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Rectangle : public Quadrilateral {
+public:
+	Rectangle() {
+		name = "Прямоугольник";
+		a = c;
+		b = d;
+		A = B = C = D;
+	}
+	bool check() override {
+		if (a == c && b == d && A == 90 && B == 90 && C == 90 && D == 90) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Quadrant : public Quadrilateral {
+public:
+	Quadrant() {
+		name = "Квадрат";
+		a = b = c = d;
+		A = B = C = D = 90;
+	}
+	bool check() {
+		if (a == b && b == c && c == d && A == 90 && B == 90 && C == 90 && D == 90) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Parallelogram : public Quadrilateral {
+public:
+	Parallelogram() {
+		name = "Параллелограмм";
+		a = c;
+		b = d;
+		A = C;
+		B = D;
+	}
+	bool check() {
+		if (a == c && b == d && A == C && B == D) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+class Rhomb : public Quadrilateral {
+public:
+	Rhomb() {
+		name = "Ромб";
+		a = b = c = d;
+		A = C;
+		B = D;
+	}
+	bool check() {
+		if (a == b && b == c && c == d && A == C && B == D) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	Figure fig;
+	Figure* ptr_fig = &fig;
+	ptr_fig->print_info();
+
+	Triangle tr;
+	Figure* ptr_tr = &tr;
+	ptr_tr->print_info();
+
+	Right_angled_triangle rat;
+	Figure* ptr_rat = &rat;
+	ptr_rat->print_info();
+
+	Isosceles_triangle it;
+	Figure* ptr_it = &it;
+	ptr_it->print_info();
+
+	Equilateral_triangle et;
+	Figure* ptr_et = &et;
+	ptr_et->print_info();
+
+	Quadrilateral quadr;
+	Figure* ptr_quadr = &quadr;
+	ptr_quadr->print_info();
+
+	Rectangle rec;
+	Figure* ptr_rec = &rec;
+	ptr_rec->print_info();
+
+	Quadrant q;
+	Figure* ptr_q = &q;
+	ptr_q->print_info();
+
+	Parallelogram par;
+	Figure* ptr_par = &par;
+	ptr_par->print_info();
+
+	Rhomb rhomb;
+	Figure* ptr_rhomb = &rhomb;
+	ptr_rhomb->print_info();
+
+
+	return EXIT_SUCCESS;
+}
