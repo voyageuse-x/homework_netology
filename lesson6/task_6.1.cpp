5eb08e7d360b92575dab0b887d4606f19379c7f9
Xenia <xenia.sytch@icloud.com>
2025-03-08 21:56:40 +0300

    Added tasks_6

---
+++ lesson6/task_6.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#define MODE 1
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	
+#ifndef MODE
+#error "Необходимо определить MODE"
+	//std::cout << "Необходимо определить MODE";
+#elif MODE==0
+	std::cout << "Работаю в тестовом режиме";
+#elif MODE==1
+	int a;
+	int b;
+	int add(int a, int b);
+	std::cout << "Работаю в боевом режиме" << std::endl;
+	std::cout << "Введите число 1: ";
+	std::cin >> a;
+	std::cout << "Введите число 2: ";
+	std::cin >> b;
+	std::cout << "Результат сложения: ";
+	std::cout << add(a, b);
+#else
+	std::cout << "Неизвестный режим. Завершение работы";
+
+#endif // !MODE{}
+	return 0;
+}
+int add(int a, int b) {
+	return a + b;
+}
